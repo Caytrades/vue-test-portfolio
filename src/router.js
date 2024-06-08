42ee51b2341e89d9180401bbea736fce619405ed
Caytrades <nanakwameopokuababio@gmail.com>
2024-06-07 17:14:30 -0700

    Updated

---
+++ src/router.js
@@ -0,0 +1,20 @@
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+// import Projects from './components/Projects.vue';
+// import AboutPage from './components/AboutPage.vue';
+
+// Vue.use(VueRouter);
+
+// const routes = [
+//   { path: '/', component: Projects },
+//   { path: '/about', component: AboutPage }
+// ];
+
+// const router = new VueRouter({
+// // mode: 'history', // optional, for cleaner URLs
+// base: process.env.BASE_URL,
+//  routes
+// })
+
+// export default router
+              
